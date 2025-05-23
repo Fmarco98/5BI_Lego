1cd43c1e16235078a1b18a6397bb2994f138cfc3
Fmarco98 <150373462+Fmarco98@users.noreply.github.com>
2025-05-23 23:08:12 +0200

    test update

--- test/mqtt.py
+++ test/mqtt.py
@@ -1,12 +1,17 @@
 import paho.mqtt.client as mqtt
 import time
+import cripta
+
+c = None
 
 def oc(client, userdata, flags, rc, properties):
     if rc == 0:
         print('connesso')
 
+    c.subscribe('iiseuganeoprojects/escavatore/ruspa-1/misure')
+
 def om(client, userdata, msg):
-    print(msg.payload.decode('utf-8'))
+    print(cripta.decriptazione(msg.payload.decode('utf-8'), 'ABCD'))
 
 c = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
 c.on_connect = oc
@@ -16,7 +21,7 @@ c.connect('broker.hivemq.com', 1883, 600)
 
 c.loop_start()
 
-c.publish('iiseuganeoprojects/escavatore/ruspa-1/comandi', 'msg1', 0)
+c.publish('iiseuganeoprojects/escavatore/ruspa-1/comandi', cripta.criptazione('{"aaaa": 1}', 'ABCD'), 0)
 
 while True:
     time.sleep(0.1)
